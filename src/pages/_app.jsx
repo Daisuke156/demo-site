95aec8a5e4a3870857cbdba447b82e9d48aa249e
jungle379 <jungle379@gmail.com>
2022-06-23 20:38:47 +0900

    ⚡️

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,37 @@
 import "tailwindcss/tailwind.css";
+import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const publicPages = [];
+
+function MyApp({ Component, pageProps }) {
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+
+  // If the current route is listed as public, render it directly
+  // Otherwise, use Clerk to require authentication
+  return (
+    <>
+      {/* <ClerkProvider> */}
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <div>
+            {/* <SignedIn> */}
+            <Component {...pageProps} />
+            {/* </SignedIn> */}
+          </div>
+          {/* <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut> */}
+        </>
+      )}
+      {/* </ClerkProvider> */}
+    </>
+  );
 }
+
+export default MyApp;
