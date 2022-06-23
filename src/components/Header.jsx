f67b744436193b4a67386398a2c763173895e553
jungle379 <jungle379@gmail.com>
2022-06-23 19:21:35 +0900

    ⚡️

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <div className="text-red-300 text-4xl bg-red-400">Demo Site</div>
-      <div className="flex">
+      <div className="flex justify-between bg-green-400">
         <div>
           <Link href="posts/news">
             <a>最新情報</a>
@@ -12,7 +12,7 @@ const Header = () => {
         </div>
         <div>
           <Link href="posts/policy">
-            <a>〇〇の理念</a>
+            <a>**の理念</a>
           </Link>
         </div>
         <div>
