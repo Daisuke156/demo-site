926ad6377de1500702e681e54567f9d05e83b0eb
jungle379 <jungle379@gmail.com>
2022-07-03 05:08:44 +0900

    ⚡️

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,33 +23,33 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-evenly text-2xl py-5 bg-red-700 text-white">
-        <div className="px-10 border-x-2 border-x-black">
+      <div className="flex justify-center text-2xl py-5 bg-red-700 text-white">
+        <div className="px-10 border-x-2  border-x-black">
           <Link href="posts/news">
             <a>最新情報</a>
           </Link>
         </div>
-        <div className="pr-10 border-r-2 border-r-black">
+        <div className="px-10">
           <Link href="posts/policy">
             <a>**の理念</a>
           </Link>
         </div>
-        <div className="pr-10 border-r-2 border-r-black">
+        <div className="px-10 border-x-2 border-x-black">
           <Link href="posts/service">
             <a>業務内容</a>
           </Link>
         </div>
-        <div className="pr-10 border-r-2 border-r-black">
+        <div className="px-10">
           <Link href="posts/company">
             <a>会社概要</a>
           </Link>
         </div>
-        <div className="pr-10 border-r-2 border-r-black">
+        <div className="px-10 border-x-2 border-x-black">
           <Link href="posts/recruit">
             <a>採用情報</a>
           </Link>
         </div>
-        <div className="pr-10 border-r-2 border-r-black">
+        <div className="px-10 border-r-2 border-r-black">
           <Link href="posts/contact">
             <a>お問い合わせ</a>
           </Link>
