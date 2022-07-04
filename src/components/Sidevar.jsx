529ba00d111f9e2044857e0989b28f8e1133a6c5
jungle379 <jungle379@gmail.com>
2022-07-05 03:48:11 +0900

    ⚡️

--- src/components/Sidevar.jsx
+++ src/components/Sidevar.jsx
@@ -2,11 +2,11 @@ const Sidevar = () => {
   return (
     <>
       <div className="h-screen">
-        <div className="h-1/3">最新情報</div>
+        <div className="h-1/3 bg-pink-400">最新情報</div>
         <div></div>
         <div></div>
-        <div className="h-1/3">事務所紹介</div>
-        <div className="h-1/3">Pマーク</div>
+        <div className="h-1/3 bg-lime-300">事務所紹介</div>
+        <div className="h-1/3 bg-blue-400">Pマーク</div>
       </div>
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,19 +12,21 @@ const Home = () => {
         <title>Demo Site</title>
       </Head>
       <Header />
-      <main className="flex h-screen">
-        <div className="w-1/4">
-          <Sidevar />
+      <main className="h-auto">
+        <div className="flex">
+          <div className="w-1/4">
+            <Sidevar />
+          </div>
+          <div className="w-2/4 h-screen">
+            <h1 className="h-1/5">時空を・・・</h1>
+            <h1 className="h-1/5">業務内容</h1>
+            <h2 className="h-1/5">ロゴ</h2>
+            <h3 className="h-1/5">
+              株式会社**はシステム構築、システム運用を主業務としています。システムエンジニアをやる気のある方を採用募集しております。東京、名古屋、神戸と全国に展開しています。フリーダイヤル:0120-111-222
+            </h3>
+          </div>
         </div>
-        <div className="w-2/4 h-screen">
-          <h1 className="h-1/5">時空を・・・</h1>
-          <h1 className="h-1/5">業務内容</h1>
-          <h2 className="h-1/5">ロゴ</h2>
-          <h3 className="h-1/5">
-            株式会社**はシステム構築、システム運用を主業務としています。システムエンジニアをやる気のある方を採用募集しております。東京、名古屋、神戸と全国に展開しています。フリーダイヤル:0120-111-222
-          </h3>
-        </div>
-        <div className="w-1/4 flex justify-end">
+        <div className="flex justify-end h-3/5">
           <ReturnTopButton />
         </div>
       </main>
