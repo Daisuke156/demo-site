01988152aeda8e4c43542ce565ffc4ae6392db79
jungle379 <jungle379@gmail.com>
2022-07-07 19:06:09 +0900

    🐛

---
+++ src/pages/posts/branch.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const Branch = () => {
+  return (
+    <>
+      <div>
+        <Link href="/">
+          <a>東京支店</a>
+        </Link>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </>
+  );
+};
+export default Branch;
