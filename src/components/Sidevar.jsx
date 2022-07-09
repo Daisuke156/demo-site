1c0e1f3a00216f7bfa49aa8c3f7924c03cecc6af
jungle379 <jungle379@gmail.com>
2022-07-09 09:19:07 +0900

    ⚡️

--- src/components/Sidevar.jsx
+++ src/components/Sidevar.jsx
@@ -1,3 +1,5 @@
+import Branch from "../pages/posts/branch";
+
 const Sidevar = () => {
   return (
     <>
@@ -5,7 +7,10 @@ const Sidevar = () => {
         <div className="h-1/3 bg-pink-400">最新情報</div>
         <div></div>
         <div></div>
-        <div className="h-1/3 bg-lime-300">事務所紹介</div>
+        <div className="h-1/3 bg-lime-300">
+          事務所紹介
+          <Branch />
+        </div>
         <div className="h-1/3 bg-blue-400">Pマーク</div>
       </div>
     </>
