301fd3858cc4c5fa16c42093634ee5cd3b729a57
jungle379 <jungle379@gmail.com>
2022-07-09 16:42:59 +0900

    ⚡️ syuusei

--- src/components/Sidevar.jsx
+++ src/components/Sidevar.jsx
@@ -5,7 +5,7 @@ const Sidevar = () => {
   return (
     <>
       <div className="h-screen">
-        <div className="h-1/3 flex justify-between">
+        <div className="h-1/4 flex justify-between">
           <div className="">最新情報</div>
           <div className="pr-10">
             <Link href="/">
@@ -13,11 +13,11 @@ const Sidevar = () => {
             </Link>
           </div>
         </div>
-        <div className="h-2/3 bg-lime-300">
+        <div className="h-2/4 bg-lime-300">
           事務所紹介
           <Branch />
         </div>
-        <div className=" bg-blue-400">Pマーク</div>
+        <div className="h-1/4 bg-blue-400">Pマーク</div>
       </div>
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,16 +14,21 @@ const Home = () => {
       <Header />
       <main className="h-auto">
         <div className="flex">
-          <div className="w-1/4">
+          <div className="w-1/6">
             <Sidevar />
           </div>
-          <div className="w-2/4 h-screen">
+          <div className="w-5/6 h-screen">
             <h1 className="h-1/5">時空を超えた・・・</h1>
             <h1 className="h-1/5">業務内容</h1>
             <h2 className="h-1/5">ロゴ</h2>
             <h3 className="h-1/5">
-              **はシステム構築、システム運用を主業務としています。システムエンジニアをやる気のある方を採用募集しております。東京、名古屋、神戸と全国に展開しています。フリーダイヤル:0120-111-222
+              **はシステム構築、システム運用を主業務としています。
+              <br />
+              システムエンジニアをやる気のある方を採用募集しております。東京、名古屋、神戸と全国に展開しています。
+              <br />
+              フリーダイヤル:0120-111-222
             </h3>
+            <h4></h4>
           </div>
         </div>
         <div className="flex justify-end h-3/5 bg-gray-200 pr-20">
