95d02a0ac442a79d909949caa06766355e3bb8c2
jungle379 <jungle379@gmail.com>
2022-07-09 16:33:50 +0900

    ⚡️

--- src/components/Sidevar.jsx
+++ src/components/Sidevar.jsx
@@ -1,17 +1,23 @@
+import Link from "next/link";
 import Branch from "../pages/posts/branch";
 
 const Sidevar = () => {
   return (
     <>
       <div className="h-screen">
-        <div className="h-1/3 bg-pink-400">最新情報</div>
-        <div></div>
-        <div></div>
-        <div className="h-1/3 bg-lime-300">
+        <div className="h-1/3 flex justify-between">
+          <div className="">最新情報</div>
+          <div className="pr-10">
+            <Link href="/">
+              <a>一覧</a>
+            </Link>
+          </div>
+        </div>
+        <div className="h-2/3 bg-lime-300">
           事務所紹介
           <Branch />
         </div>
-        <div className="h-1/3 bg-blue-400">Pマーク</div>
+        <div className=" bg-blue-400">Pマーク</div>
       </div>
     </>
   );
