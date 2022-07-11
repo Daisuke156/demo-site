9cdb557edbcd117fa80a7e801a90a0362095b5fa
jungle379 <jungle379@gmail.com>
2022-07-12 03:41:08 +0900

    🔥

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,15 +18,15 @@ const Home = () => {
           </div>
           <div className="w-5/6 h-screen bg-blue-100">
             <h1 className="h-1/5">時空を超えた・・・</h1>
-            <h1 className="h-1/5">業務内容</h1>
-            <h2 className="h-1/5">ロゴ</h2>
-            <h3 className="h-1/5">
+            <h2 className="h-1/5">業務内容</h2>
+            <h3 className="h-1/5">ロゴ</h3>
+            <h4 className="h-1/5">
               **はシステム構築、システム運用を主業務としています。
               <br />
               システムエンジニアをやる気のある方を採用募集しております。東京、名古屋、神戸と全国に展開しています。
               <br />
               フリーダイヤル:0120-111-222
-            </h3>
+            </h4>
           </div>
         </div>
       </main>
