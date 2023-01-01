10512c20279731ba6a89f706baa160b71281153e
jungle379 <jungle379@gmail.com>
2023-01-01 19:02:36 +0900

    🔥

--- src/components/branch.jsx
+++ src/components/branch.jsx
@@ -4,7 +4,7 @@ const Branch = () => {
   return (
     <>
       <div>
-        <div className="text-xl">
+        <div className="text-xl hover:underline">
           <Link href="/office/Tokyo">東京事務所(本部)</Link>
         </div>
         <h1>〒105-0000</h1>
@@ -12,7 +12,7 @@ const Branch = () => {
         <h3>03-1111-2222(代)</h3>
       </div>
       <div className="my-5">
-        <div className="text-xl">
+        <div className="text-xl hover:underline">
           <Link href="/office/Osaka">大阪事務所</Link>
         </div>
         <h1>〒562-0000</h1>
@@ -20,7 +20,7 @@ const Branch = () => {
         <h3>06-1111-2222</h3>
       </div>
       <div>
-        <div className="text-xl">
+        <div className="text-xl hover:underline">
           <Link href="/office/Nagoya">名古屋事務所</Link>
         </div>
         <h1>〒460-0000</h1>
