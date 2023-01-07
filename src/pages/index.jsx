56ddc5f3257ff2a509e47c3d253e2f8fa69c9431
jungle379 <jungle379@gmail.com>
2023-01-07 13:12:09 +0900

    🔥

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Sidevar from "../components/Sidevar";
-import { Carousel } from "@mantine/carousel";
 
 const Home = () => {
   return (
@@ -20,21 +19,6 @@ const Home = () => {
           <div className="w-5/6">
             <div className="h-[480px] bg-red-50">
               <div className="text-4xl py-10 px-10 font-bold">業務内容</div>
-              {/* <div className="my-10 bg-blue-50">
-                <Carousel
-                  slideSize="70%"
-                  height={300}
-                  slideGap="md"
-                  loop
-                  withIndicators
-                >
-                  <Carousel.Slide>1</Carousel.Slide>
-                  <Carousel.Slide>2</Carousel.Slide>
-                  <Carousel.Slide>3</Carousel.Slide>
-                  <Carousel.Slide>4</Carousel.Slide>
-                  <Carousel.Slide>5</Carousel.Slide>
-                </Carousel>
-              </div> */}
             </div>
             <div className="px-10 h-[400px] bg-red-50">
               システム構築、運用を主業務としています。
