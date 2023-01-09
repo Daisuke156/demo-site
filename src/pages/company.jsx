a3b8d42219fed2c7d152da4e08833a8f52705bf9
jungle379 <jungle379@gmail.com>
2023-01-09 19:19:13 +0900

    🔥

--- src/pages/company.jsx
+++ src/pages/company.jsx
@@ -19,32 +19,38 @@ const Company = () => {
         <div className="w-5/6 px-20">
           <div className="text-4xl my-10 font-bold">会社概要</div>
           <div>
-            <div className="flex my-10 text-2xl">
-              <h1>会社名：</h1>
-              <h2>株式会社**</h2>
+            <div className="flex justify-between">
+              <div>
+                <div className="flex text-2xl">
+                  <h1>会社名：</h1>
+                  <h2>株式会社**</h2>
+                </div>
+                <div className="my-10">
+                  <h1>設立:1987年6月</h1>
+                </div>
+                <div className="my-10">
+                  <h1>営業種目</h1>
+                </div>
+                <div className="my-10">
+                  <h1>年商:1億円</h1>
+                </div>
+                <div className="my-10">
+                  <h1>資本金:1000万円</h1>
+                </div>
+              </div>
+              <div>
+                <h1>所在地:東京都港区</h1>
+                <div className="h-[300px] w-[300px]">
+                  <AspectRatio ratio={100 / 100}>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
+                      title="Google map"
+                    />
+                  </AspectRatio>
+                </div>
+              </div>
             </div>
-            <h1>所在地:東京都港区</h1>
-            <div className="h-[300px] w-[300px]">
-              <AspectRatio ratio={100 / 100}>
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
-                  title="Google map"
-                />
-              </AspectRatio>
-            </div>
-            <div className="my-10">
-              <h1>設立:1987年6月</h1>
-            </div>
-            <div className="my-10">
-              <h1>営業種目</h1>
-            </div>
-            <div className="my-10">
-              <h1>年商:1億円</h1>
-            </div>
-            <div className="my-10">
-              <h1>資本金:1000万円</h1>
-            </div>
-            <div className="my-10">
+            <div>
               <h1>代表取締役</h1>
             </div>
             <div className="my-10">
@@ -58,13 +64,13 @@ const Company = () => {
             </div>
             <div className="my-10">
               <h1>取引先</h1>
-              <div className="hover:text-red-500">
+              <div className="hover:text-red-500 my-5">
                 <Link href="/">※※※株式会社</Link>
               </div>
-              <div className="hover:text-red-500">
+              <div className="hover:text-red-500 my-5">
                 <Link href="/">株式会社***</Link>
               </div>
-              <div className="hover:text-red-500">
+              <div className="hover:text-red-500 my-5">
                 <Link href="/">※※※株式会社</Link>
               </div>
             </div>
