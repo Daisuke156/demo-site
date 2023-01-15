93a2de0f584941b2c7ef7389c8111dd0b7da11bc
jungle379 <jungle379@gmail.com>
2023-01-15 14:44:54 +0900

    🔥

--- src/pages/branch.jsx
+++ src/pages/branch.jsx
@@ -18,36 +18,42 @@ const Branch = () => {
           </div>
           <div className="mx-10 my-10 w-5/6">
             <div className="font-bold text-4xl pb-20">各事業所紹介</div>
-            <div className="mb-20 px-10">
+            <div className="mb-10 px-10">
               当社の事業所は全国にございます。詳細は以下のリンクからご確認ください。
             </div>
-            <div className="mx-40">
-              <div className="font-bold text-xl">東京事業所(本部)</div>
-              <div className="h-[300px] w-[300px] my-10">
-                <AspectRatio ratio={100 / 100}>
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
-                    title="Google map"
-                  />
-                </AspectRatio>
+            <div className="mx-20">
+              <div className="my-10">
+                <div className="font-bold text-xl">東京事業所(本部)</div>
+                <div className="h-[400px] w-[400px]">
+                  <AspectRatio ratio={100 / 100}>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
+                      title="Google map"
+                    />
+                  </AspectRatio>
+                </div>
               </div>
-              <div className="font-bold text-xl">大阪事業所</div>
-              <div className="h-[300px] w-[300px] my-10">
-                <AspectRatio ratio={100 / 100}>
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
-                    title="Google map"
-                  />
-                </AspectRatio>
+              <div className="my-10">
+                <div className="font-bold text-xl">大阪事業所</div>
+                <div className="h-[400px] w-[400px]">
+                  <AspectRatio ratio={100 / 100}>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
+                      title="Google map"
+                    />
+                  </AspectRatio>
+                </div>
               </div>
-              <div className="font-bold text-xl">名古屋事業所</div>
-              <div className="h-[300px] w-[300px] my-10">
-                <AspectRatio ratio={100 / 100}>
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
-                    title="Google map"
-                  />
-                </AspectRatio>
+              <div className="my-10">
+                <div className="font-bold text-xl">名古屋事業所</div>
+                <div className="h-[400px] w-[400px]">
+                  <AspectRatio ratio={100 / 100}>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.3063874233135!2d-74.04668908358428!3d40.68924937933441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25090129c363d%3A0x40c6a5770d25022b!2sStatue%20of%20Liberty%20National%20Monument!5e0!3m2!1sen!2sru!4v1644262070010!5m2!1sen!2sru"
+                      title="Google map"
+                    />
+                  </AspectRatio>
+                </div>
               </div>
             </div>
           </div>
