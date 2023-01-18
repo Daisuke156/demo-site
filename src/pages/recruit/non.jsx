f85b371e3c2b054e9b394bcf2e80cc3b275c1c33
jungle379 <jungle379@gmail.com>
2023-01-18 18:30:20 +0900

    ⚡️

--- src/pages/recruit/non.jsx
+++ src/pages/recruit/non.jsx
@@ -13,29 +13,31 @@ const non = () => {
         <title>応募フォーム(未経験)</title>
       </Head>
       <Header />
-      <div className="flex h-screen">
+      <div className="flex h-full">
         <div className="w-1/6">
           <Sidevar />
         </div>
         <div className="w-5/6 px-20 bg-lime-100">
           <div>
             <div className="my-10 text-4xl font-bold">応募フォーム</div>
-            <div className="mb-10  font-bold text-2xl">未経験の方</div>
-            <div className="bg-blue-500 w-[100px]">
-              <Modal
-                opened={opened}
-                onClose={() => setOpened(false)}
-                title="Introduce yourself!"
-              >
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
-                aliquam amet inventore aspernatur eum in harum dolores aliquid
-                provident sapiente perspiciatis officia, similique tempora. Ab,
-                eius velit. Impedit, maxime cupiditate?
-              </Modal>
+            <div className="mx-10">
+              <div className="mb-10  font-bold text-2xl">未経験の方</div>
+              <div className="bg-blue-500 w-[100px]">
+                <Modal
+                  opened={opened}
+                  onClose={() => setOpened(false)}
+                  title="Introduce yourself!"
+                >
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Velit aliquam amet inventore aspernatur eum in harum dolores
+                  aliquid provident sapiente perspiciatis officia, similique
+                  tempora. Ab, eius velit. Impedit, maxime cupiditate?
+                </Modal>
 
-              <Group position="center">
-                <Button onClick={() => setOpened(true)}>募集要項</Button>
-              </Group>
+                <Group position="center">
+                  <Button onClick={() => setOpened(true)}>募集要項</Button>
+                </Group>
+              </div>
             </div>
           </div>
         </div>
