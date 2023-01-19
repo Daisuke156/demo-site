27d00490395779d54a66e33df1bdb5e2d3d9e13c
jungle379 <jungle379@gmail.com>
2023-01-19 22:09:53 +0900

    ⚡️

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -19,10 +19,19 @@ const Service = () => {
           <div className="mb-10 text-4xl font-bold">業務内容</div>
           <div className="flex justify-evenly">
             <div>
-              <div className="my-10 text-2xl">開発</div>
-              <h1></h1>
-              <h2></h2>
-              <h3></h3>
+              <div className="my-5 text-2xl">開発</div>
+              <h1 className="text-xl">
+                フロントエンド・バックエンドエンジニア
+              </h1>
+              <h2 className="mt-5">使用言語</h2>
+              <div className="mx-5 my-5">
+                <li>JavaScript</li>
+                <li>PHP</li>
+                <li>GraphQL</li>
+              </div>
+              <h3 className="mb-10">
+                主に自社内で、受託開発を行なっています。
+              </h3>
               <div>
                 <Card shadow="sm" p="lg" radius="md" withBorder>
                   <Card.Section>
@@ -36,10 +45,16 @@ const Service = () => {
               </div>
             </div>
             <div>
-              <div className="my-10 text-2xl">業務系</div>
-              <h1></h1>
-              <h2></h2>
-              <h3></h3>
+              <div className="my-5 text-2xl">業務系</div>
+              <h1 className="text-xl">業務システム構築・保守</h1>
+              <h2 className="mt-5">使用言語</h2>
+              <div className="mx-5 mt-5 mb-10">
+                <li>Java</li>
+                <li>C#</li>
+              </div>
+              <h3 className="mb-10">
+                お客様先に常駐し、システム構築をメインに行なっています
+              </h3>
               <div>
                 <Card shadow="sm" p="lg" radius="md" withBorder>
                   <Card.Section>
@@ -54,11 +69,13 @@ const Service = () => {
             </div>
           </div>
           <div className="flex justify-evenly">
-            <div>
+            <div className="mx-10">
               <div className="my-10 text-2xl">ヘルプデスク</div>
-              <h1></h1>
-              <h2></h2>
-              <h3></h3>
+              <h1 className="text-xl">ヘルプデスク業務</h1>
+              <h2 className="mt-5"></h2>
+              <h3 className="mb-10">
+                お客様先に常駐し、キッティングやお客様からの依頼対応を行います。
+              </h3>
               <div>
                 <Card shadow="sm" p="lg" radius="md" withBorder>
                   <Card.Section>
@@ -73,9 +90,11 @@ const Service = () => {
             </div>
             <div>
               <div className="my-10 text-2xl">オペレータ</div>
-              <h1></h1>
-              <h2></h2>
-              <h3></h3>
+              <h1 className="text-xl">オペレータ業務</h1>
+              <h2 className="mt-5"></h2>
+              <h3 className="mb-10">
+                お客様先に常駐し、指示書に従いサーバやマシンの運用・監視を行います。
+              </h3>
               <div>
                 <Card shadow="sm" p="lg" radius="md" withBorder>
                   <Card.Section>
@@ -89,6 +108,7 @@ const Service = () => {
               </div>
             </div>
           </div>
+          <div></div>
         </div>
       </div>
       <Footer />
