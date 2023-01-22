03ae1b5b68b67809f4c1889571834d9008e98343
jungle379 <jungle379@gmail.com>
2023-01-23 06:13:25 +0900

    🔥

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -203,7 +203,7 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            <div className="my-[8px] py-5 mx-10 border-2 border-red-100 flex justify-between">
+            <div className="my-[8px] py-5 px-10 mx-10 border-2 border-red-100 flex justify-between">
               <div>
                 <div className="text-xl">株式会社***</div>
                 <div>東京事務所(本部)</div>
