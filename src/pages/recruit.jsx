02fcb25040332031649f8422eeab2c001a9d0538
jungle379 <jungle379@gmail.com>
2023-02-12 02:18:45 +0900

    🐛

--- src/pages/recruit.jsx
+++ src/pages/recruit.jsx
@@ -47,7 +47,7 @@ const Recruit = () => {
           </div>
           <div className="mx-5 border-y-2 border-x-2">
             <div className="flex">
-              <div>
+              <div className="w-[300px]">
                 <div className="font-bold border-2 border-x-2 border-y-2">
                   雇用形態
                 </div>
@@ -60,7 +60,7 @@ const Recruit = () => {
                 <div className="font-bold border-2 border-x-2 border-y-2 h-[150px]">
                   勤務地
                 </div>
-                <div className="font-bold border-2 border-x-2 border-y-2 h-[300px]">
+                <div className="font-bold border-2 border-x-2 border-y-2 h-[500px]">
                   勤務時間
                 </div>
                 <div className="font-bold border-2 border-x-2 border-y-2">
@@ -70,9 +70,9 @@ const Recruit = () => {
                   採用予定人数
                 </div>
                 <div className="font-bold border-2 border-x-2 border-y-2">
-                  勤務時間
+                  勤務期間
                 </div>
-                <div className="font-bold border-2 border-x-2 border-y-2 h-[50px]">
+                <div className="font-bold border-2 border-x-2 border-y-2 h-[70px]">
                   待遇
                 </div>
               </div>
@@ -105,14 +105,89 @@ const Recruit = () => {
                   <h2>兵庫県（神戸市/西神中央駅）</h2>
                   <h2>愛知県（名古屋市/新栄町駅）</h2>
                 </div>
-                <div className="h-[300px] border-2 border-x-2 border-y-2"></div>
-                <div className="text-sm border-2 border-x-2 border-y-2"></div>
-                <div className="text-sm border-2 border-x-2 border-y-2"></div>
-                <div className="text-sm border-2 border-x-2 border-y-2"></div>
+                <div className="h-[500px] text-sm border-2 border-x-2 border-y-2">
+                  <h1 className="text-red-500 font-bold">●勤務時間</h1>
+                  <h2>■サービスデスク</h2>
+                  <h3>
+                    神奈川県（武蔵小杉駅）
+                    <br /> 三交代 8:30～17:30、15:00～21:30、21:00～翌9:00
+                    <br />
+                    ※24時間365日運用、交代制(休日等で二交代あり）
+                    <br />
+                    ※休日交代制（月7～10日）
+                    <br />
+                  </h3>
+                  <h2>■オペレータ</h2>
+                  <h3>
+                    神奈川県（センター北駅）
+                    <br />
+                    三交代 8:30～17:30、13:00～22:00、21:45～翌8:45
+                    <br />
+                    ※24時間365日運用
+                    <br />
+                    ※休日交代制（月7～10日）
+                  </h3>
+                  <h3>
+                    兵庫県（西神中央駅）
+                    <br />
+                    二交替 9:00～21:15 21:00～翌9:15
+                    <br />
+                    三交替 9:00～16:15 16:00～21:15 21:00～翌8:45
+                    <br />
+                    ※24時間365日運用 交代制(二交替メイン、一部三交代あり)
+                    <br />
+                    ※休日交代制（月8～12日）
+                  </h3>
+                  <h3></h3>愛知県（新栄町駅）
+                  <br />
+                  二交代 8:00～20:30 20:15～翌8:15
+                  <br />
+                  ※24時間365日運用 交代制
+                  <br />
+                  ※休日交代制（月8～11日）
+                  <h1 className="text-red-500 font-bold">
+                    ●その他の休日・休暇
+                  </h1>
+                  <h2>慶弔、年次有給休暇</h2>
+                </div>
+                <div className="h-[28px] text-sm border-2 border-x-2 border-y-2">
+                  学歴不問、学生不可 未経験の方、歓迎します！
+                </div>
+                <div className="h-[28px] text-sm border-2 border-x-2 border-y-2">
+                  各部署 2～4名程度
+                </div>
+                <div className="h-[28px] text-sm border-2 border-x-2 border-y-2">
+                  長期続けていただける方を希望します。
+                </div>
+                <div className="h-[70px] text-sm border-2 border-x-2 border-y-2">
+                  <h1>
+                    交通費全額支給
+                    <br />
+                    社会保険完備
+                    <br />
+                    正社員登用あり
+                  </h1>
+                </div>
               </div>
             </div>
           </div>
-          <div className="my-80">
+          <div className="my-5 border-2 border-x-2 border-y-2">
+            <h1 className="mt-5 ml-5 text-xl text-red-700">職場情報</h1>
+            <h2 className="text-sm my-5 ml-5">
+              流通システムを支える大型汎用機やサーバの監視、マシンの操作から
+              <br />
+              はじまり、日本有数の大手企業のネットワーク環境を整えたり、監視
+              <br />
+              の仕事を楽にするためのシステムを開発したりと、運用と一体化した
+              <br />
+              トータルソリューションに取り組んでいます。
+            </h2>
+            <h2 className="text-sm ml-5">■お問い合わせはこちらまで</h2>
+            <h3 className="text-sm mb-5 ml-5">
+              フリーダイヤル 0120-**:-*** / TEL 03-****-****
+            </h3>
+          </div>
+          <div className="my-10">
             <Info />
           </div>
         </div>
